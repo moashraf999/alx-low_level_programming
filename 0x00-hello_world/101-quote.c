d93df50ba9b923e0524429d4e645c55aae29ee2e
moashraf999 <moh.ashraf182004@gmail.com>
2023-10-07 23:26:47 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point (prints quote)
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
