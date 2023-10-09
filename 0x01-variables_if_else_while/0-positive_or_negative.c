95d6a43c57eb8c27357578986e0b69cdbc91b122
moashraf999 <moh.ashraf182004@gmail.com>
2023-10-08 17:39:13 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Returns value of random number followed by positive or negative
+ *main - Returns value of random number followed by positive or negative
  *
- * Return: Always 0(Success) 
+ *Return: Always 0(Success) 
 */
 int main(void)
 {
